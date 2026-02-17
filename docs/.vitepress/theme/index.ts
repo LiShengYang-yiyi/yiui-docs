e2afaebbd667fd69892b40d0c0604fa31074b53f
LiShengYang-yiyi <379338943@qq.com>
2026-02-17 17:48:56 +0800

    Reapply "feat: 集成 ui-ux-pro-max-skill 并添加自定义主题"
    This reverts commit 91a07069f12badafd7b9792fe06f059881588982.

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,11 @@
+// YIUI Docs Custom Theme
+// Based on UI-UX-Pro-Max-Skill: Developer Tool + Gaming Theme
+import DefaultTheme from 'vitepress/theme'
+import './custom.css'
+
+export default {
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    // 可以在这里注册全局组件
+  }
+}
